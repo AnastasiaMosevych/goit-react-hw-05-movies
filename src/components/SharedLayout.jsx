f7c2915e3e575b567a1a4fdf2ec5ddbab22062df
AnastasiaMosevych <ukrainischesmaedel@yahoo.de>
2023-04-04 23:16:14 +0200

    added styles to header

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -1,22 +1,24 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { NavContainer,Header, NavList, NavListItem, StyledLink } from "./SharedLayout.styled";
 import { Suspense } from "react";
 
 export const SharedLayout = () => {
     return (
         <div>
-            <header>
-                <nav>
-                    <ul>
-                        <li>
-                            <NavLink to="/">Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/movies">Movies</NavLink>
-                        </li>
-                    </ul>
-                </nav>
-                    
-            </header>
+            <Header>
+                <NavContainer>
+                    <nav>
+                        <NavList>
+                            <NavListItem>
+                                <StyledLink to="/">Home</StyledLink>
+                            </NavListItem>
+                            <NavListItem>
+                                <StyledLink to="/movies">Movies</StyledLink>
+                            </NavListItem>
+                        </NavList>
+                    </nav>
+                </NavContainer>         
+            </Header>
             <main>
                 <Suspense fallback={<h1>Loading...</h1>}>
                     <Outlet />
